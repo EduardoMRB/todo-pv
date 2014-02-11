9528f086a432cde0797c3416b93a4d9be2cfc143
Eduardo Borges <eduardomrb@gmail.com>
2014-02-10 21:43:15 -0300

    Adiciona gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('scripts', function () {
+  gulp.src('public/assets/js/*.js, bower_components/*/*.min.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('public/assets'))
+    .pipe(rename('compiled.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets'));
+});
+
+gulp.task('styles', function () {
+  gulp.src('public/assets/css/*.css, bower_components/*/*.min.css')
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('public/assets'))
+    .pipe(rename('styles.css'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('public/assets/js/*.js', 'bower_components/*/*.min.js', ['scripts']);
+}); 
+
+gulp.task('default', ['scripts', 'watch', 'styles']);
